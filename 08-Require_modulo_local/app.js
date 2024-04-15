6cf27dca537ee520dc164c69984f4cff0adaa2af
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-04-15 12:44:27 -0400

    fifth commit Node.js

---
+++ 08-Require_modulo_local/app.js
@@ -0,0 +1,13 @@
+var miModulo = require('./miModulo');
+var http = require('http');
+
+
+miModulo.multiplicar(11);
+miModulo.sumar(11);
+
+
+var prueba = "10";
+var util = require('util');
+console.log( util.isString(prueba));
+
+
---
+++ 08-Require_modulo_local/miModulo.js
@@ -0,0 +1,10 @@
+var miModulo = {
+    multiplicar: function(valor1){
+        console.log( valor1 * 3)
+    },
+    sumar: function(valor1){
+        console.log( valor1 + 3)
+    }
+}
+
+module.exports = miModulo;
